6986766a9ee6172af9e552a2df4ce00c571fb673
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-03-19 18:04:03 -0300

    novos componentes e contexts

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { createRoot } from "react-dom";
-import Pizza from "./Pizza";
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import PizzaOfTheDay from "./PizzaOfTheDay";
+import Order from "./Order";
+import Header from "./Header.jsx";
+import { CartContext } from "./contexts";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
-    <div>
-      <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="A classic pizza with pepperoni"
-        image={"public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="A classic pizza with ham and pineapple"
-        image={"public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="Vegetarian"
-        description="A classic pizza with vegetables"
-        image={"public/pizzas/veggie_veg.webp"}
-      />
-      <Pizza
-        name="Meat Feast"
-        description="A classic pizza with all the meats"
-        image={"public/pizzas/big_meat.webp"}
-      />
-    </div>
-  )
-}
+    <StrictMode>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+        </div>
+      </CartContext.Provider>
+    </StrictMode>
+  );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
